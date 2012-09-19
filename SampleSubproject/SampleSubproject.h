88b7e6a5857f4d65a346dd5bb8677934192d080c
Rusty Zarse <rustyzarse-ssd@c-76-17-116-24.hsd1.ga.comcast.net>
2012-09-19 15:52:15 -0400

    Initial Commit

---
+++ SampleSubproject/SampleSubproject.h
@@ -0,0 +1,13 @@
+//
+//  SampleSubproject.h
+//  SampleSubproject
+//
+//  Created by Rusty Zarse on 9/19/12.
+//  Copyright (c) 2012 Rusty Zarse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleSubproject : NSObject
+
+@end
